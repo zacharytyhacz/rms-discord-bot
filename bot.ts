36673091cb7997777c2bd41917be55506429686f
zacharytyhacz <zactyh@gmail.com>
2026-01-07 10:59:18 -0500

    30 min timeout for RMS, better deployment steps

--- bot.ts
+++ bot.ts
@@ -12,7 +12,9 @@ const excludedChannelIds = [
 ]
 
 let lastTime = 0
-const timer = 300 * 1000 // 300 seconds
+
+// 30 minutes
+const timer = 30 * 60 * 1000;
 
 const client = new Client({
     intents: [
@@ -32,10 +34,14 @@ client.on('shardError', console.error)
 
 client.on('messageCreate', (message) => {
     const now = Date.now()
-    const timeOut = now - lastTime < timer; // A boolean indicating if the bot is allowed to send a message, based on the timer duration.
+    const timeOut = now - lastTime < timer
 
     if (!client.user || message.author.id === client.user.id) {
         return
+    }
+
+    if (timeOut) {
+      return
     }
 
     if (
@@ -45,14 +51,8 @@ client.on('messageCreate', (message) => {
         return
     }
 
-   if (/(?<!GNU\/)linux/i.test(message.content) && !message.author.bot && !timeOut)  {
-       message.channel.send(`
-I'd just like to interject for a moment. What you're referring to as Linux,
-is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux.
-Linux is not an operating system unto itself, but rather another free component
-of a fully functioning GNU system made useful by the GNU corelibs, shell
-utilities and vital system components comprising a full OS as defined by POSIX.
-`)
+   if (/(?<!GNU\/)linux/i.test(message.content) && !message.author.bot) {
+       message.channel.send("I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.")
        lastTime = now;
    }
 });
